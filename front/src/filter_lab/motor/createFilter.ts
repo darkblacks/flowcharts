b54014856efea1492df872d016adcab4f06c4bb3
darkblacks <victorferreira.s0307@gmail.com>
2026-03-26 17:15:25 -0300

    Estrutura1

--- front/src/filter_lab/motor/createFilter.ts
+++
@@ -1,9 +0,0 @@
-import ListFilter from '../type/list';
-import DateFilter from '../type/date';
-import PeriodFilter from '../type/period';
-
-export const filterRegistry = {
-  list: ListFilter,
-  date: DateFilter,
-  period: PeriodFilter,
-} as const;
